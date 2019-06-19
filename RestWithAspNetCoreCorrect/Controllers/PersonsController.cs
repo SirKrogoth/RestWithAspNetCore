e22ef98ffd6ea5a948dc8e11b58dc69fcc2fc87a
João Rafael Fagundes Menezes <joaorafael652@outlook.com>
2019-06-19 15:20:49 -0300

    Segundo Commit

---
+++ RestWithAspNetCoreCorrect/Controllers/PersonsController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestWithAspNetCore.Model;
+using RestWithAspNetCore.Services;
+
+namespace RestWithAspNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private IPersonService _personService;
+
+        public PersonsController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var person = _personService.FindById(id);
+
+            if (person == null)
+                return NotFound();
+            else
+                return Ok(person);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null)
+                return BadRequest();
+            else
+                return new ObjectResult(_personService.Create(person));
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null)
+                return BadRequest();
+            else
+                return new ObjectResult(_personService.Update(person));
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            //dddddddddddddd
+            _personService.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ RestWithAspNetCoreCorrect/Model/Context/MysqlContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAspNetCore.Model.Context
+{
+    public class MysqlContext : DbContext
+    {
+        public MysqlContext()
+        {
+
+        }
+
+        public MysqlContext(DbContextOptions<MysqlContext> options) : base(options){}
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
